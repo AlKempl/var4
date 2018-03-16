eea0fb9a35fac1648b0f6524dc38682d5d581bfc
AlKempl <alexblack191198@gmail.com>
2018-03-16 12:40:26 +0300

    Tasks 3.1-3.2 complete

--- task3.hs
+++ task3.hs
@@ -12,8 +12,33 @@
      возвращает суммарную стоимость всех партий данного товара (стоимость
      партии вычисляется как количество, умноженное на цену за единицу).
   4) (4 б.) Напишите функцию, которая по списку Item и пределу количества
-     возвращает информацию по всем партиям меньшего размера. 
+     возвращает информацию по всем партиям меньшего размера.
   5) (3 б.) Напишите основную программу, которая анализирует параметры
      командной строки, на их основе вызывает функцию из пунктов 3) или 4)
-     и печатает результаты. 
+     и печатает результаты.
 -}
+
+import System.Environment
+import Data.List
+import Data.Char
+import System.IO
+
+main = do
+  [fname] <- getArgs
+  acts <- loadData fname
+  print acts
+
+data Item = Item String Int Double
+  deriving (Show)
+
+str2item :: String -> Item
+str2item = buildItem.words
+  where
+    buildItem (title:count:price:xs) = Item title (read count) (read price)
+    buildItem _ = error "incorrect input data"
+
+
+loadData :: FilePath -> IO [Item]
+loadData fname = do
+fc <- readFile fname
+return $ map str2item $ lines fc
